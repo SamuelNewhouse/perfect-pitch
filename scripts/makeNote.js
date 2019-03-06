12270369727a5d205572afa433f143876d968dad
unknown <samuel_newhouse@yahoo.com>
2019-03-06 14:12:44 -0600

    Minor refactor for context.currentTime

--- scripts/makeNote.js
+++ scripts/makeNote.js
@@ -1,12 +1,13 @@
 const FADETIME = 0.2;
 
 const context = new (window.AudioContext || window.webkitAudioContext)();
+const now = context.currentTime;
 const compressor = context.createDynamicsCompressor();
-compressor.threshold.setValueAtTime(-50, context.currentTime);
-compressor.knee.setValueAtTime(40, context.currentTime);
-compressor.ratio.setValueAtTime(12, context.currentTime);
-compressor.attack.setValueAtTime(0, context.currentTime);
-compressor.release.setValueAtTime(1, context.currentTime);
+compressor.threshold.setValueAtTime(-50, now);
+compressor.knee.setValueAtTime(40, now);
+compressor.ratio.setValueAtTime(12, now);
+compressor.attack.setValueAtTime(0, now);
+compressor.release.setValueAtTime(1, now);
 compressor.connect(context.destination);
 
 const play = (oscillator) => {
